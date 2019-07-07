fd09cb6afcd3b5837322e9e1ba0ab63b72b55a6b
fakhrad <fakhrad@openratio.com>
2019-07-07 10:57:00 +0430

    cleaning code

---
+++ src/controllers/requestController.js
@@ -0,0 +1,198 @@
+// var jwt = require('jsonwebtoken');
+// const config = require('../config');
+// const { body, validationResult } = require('express-validator/check');
+// const { sanitizeBody } = require('express-validator/filter');
+// const broker = require('./serviceBroker');
+
+
+// exports.getAll = function(req, res, next) {
+//     broker.sendRPCMessage({spaceId : req.spaceid, userId : req.userId,  body : req.body}, 'getcontents').then((result)=>{
+//         var obj = JSON.parse(result.toString('utf8'));
+//         if (!obj.success)
+//         {
+//             if (obj.error)
+//                 return res.status(500).json(obj);
+//             else
+//             {
+//                 res.status(404).json(obj);
+//             }
+//         }
+//         else
+//         {
+//             res.status(200).json(obj.data);
+//         }
+//     });
+//   }
+
+// exports.getById = function(req, res, next) {
+//     broker.sendRPCMessage({spaceId : req.spaceid, userId : req.userId, body : {id : req.query.id}}, 'getcontentbyid').then((result)=>{
+//         var obj = JSON.parse(result.toString('utf8'));
+//         if (!obj.success)
+//         {
+//             if (obj.error)
+//                 return res.status(500).json(obj);
+//             else
+//             {
+//                 res.status(404).json(obj);
+//             }
+//         }
+//         else
+//         {
+//             res.status(200).json(obj.data);
+//         }
+//     });
+// }
+
+//   exports.add = function(req, res, next) {
+//     broker.sendRPCMessage({spaceId : req.spaceid, userId : req.userId, body : req.body}, 'addcontent').then((result)=>{
+//         var obj = JSON.parse(result.toString('utf8'));
+//         if (!obj.success)
+//         {
+//             if (obj.error)
+//                 return res.status(500).json(obj);
+//             else
+//             {
+//                 res.status(404).json(obj);
+//             }
+//         }
+//         else
+//         {
+//             res.status(200).json(obj.data);
+//         }
+//     });
+// }
+
+// exports.update = function(req, res, next) {
+//     broker.sendRPCMessage({spaceId : req.spaceid, userId : req.userId, body : req.body}, 'updatecontent').then((result)=>{
+//         var obj = JSON.parse(result.toString('utf8'));
+//         if (!obj.success)
+//         {
+//             if (obj.error)
+//                 return res.status(500).json(obj);
+//             else
+//             {
+//                 res.status(404).json(obj);
+//             }
+//         }
+//         else
+//         {
+//             res.status(200).json(obj.data);
+//         }
+//     });
+// }
+
+// exports.remove = function(req, res, next) {
+//     broker.sendRPCMessage({spaceId : req.spaceid, userId : req.userId, body : req.body}, 'removecontent').then((result)=>{
+//         var obj = JSON.parse(result.toString('utf8'));
+//         if (!obj.success)
+//         {
+//             if (obj.error)
+//                 return res.status(500).json(obj);
+//             else
+//             {
+//                 res.status(404).json(obj);
+//             }
+//         }
+//         else
+//         {
+//             res.status(200).json(obj.data);
+//         }
+//     });
+// }
+
+// exports.publish = function(req, res, next) {
+//     broker.sendRPCMessage({spaceId : req.spaceid, userId : req.userId, body : req.body}, 'publishcontent').then((result)=>{
+//         var obj = JSON.parse(result.toString('utf8'));
+//         if (!obj.success)
+//         {
+//             if (obj.error)
+//                 return res.status(500).json(obj);
+//             else
+//             {
+//                 res.status(404).json(obj);
+//             }
+//         }
+//         else
+//         {
+//             res.status(200).json(obj.data);
+//         }
+//     });
+// }
+
+// exports.unPublish = function(req, res, next) {
+//     broker.sendRPCMessage({spaceId : req.spaceid, userId : req.userId, body : req.body}, 'unpublishcontent').then((result)=>{
+//         var obj = JSON.parse(result.toString('utf8'));
+//         if (!obj.success)
+//         {
+//             if (obj.error)
+//                 return res.status(500).json(obj);
+//             else
+//             {
+//                 res.status(404).json(obj);
+//             }
+//         }
+//         else
+//         {
+//             res.status(200).json(obj.data);
+//         }
+//     });
+// }
+
+// exports.archive = function(req, res, next) {
+//     broker.sendRPCMessage({spaceId : req.spaceid, userId : req.userId, body : req.body}, 'archivecontent').then((result)=>{
+//         var obj = JSON.parse(result.toString('utf8'));
+//         if (!obj.success)
+//         {
+//             if (obj.error)
+//                 return res.status(500).json(obj);
+//             else
+//             {
+//                 res.status(404).json(obj);
+//             }
+//         }
+//         else
+//         {
+//             res.status(200).json(obj.data);
+//         }
+//     });
+// }
+
+// exports.unArchive = function(req, res, next) {
+//     broker.sendRPCMessage({spaceId : req.spaceid, userId : req.userId, body : req.body}, 'unarchivecontent').then((result)=>{
+//         var obj = JSON.parse(result.toString('utf8'));
+//         if (!obj.success)
+//         {
+//             if (obj.error)
+//                 return res.status(500).json(obj);
+//             else
+//             {
+//                 res.status(404).json(obj);
+//             }
+//         }
+//         else
+//         {
+//             res.status(200).json(obj.data);
+//         }
+//     });
+// }
+
+
+// exports.filter = function(req, res, next) {
+//     console.log(req)
+//     broker.sendRPCMessage({spaceId : req.spaceid, userId : req.userId, body : {name : req.query.name, category : req.query.category, contentType : req.query.contentType, status : req.query.status}}, 'filtercontents').then((result)=>{
+//         var obj = JSON.parse(result.toString('utf8'));
+//         if (!obj.success)
+//         {
+//             if (obj.error)
+//                 return res.status(500).json(obj);
+//             else
+//             {
+//                 res.status(404).json(obj);
+//             }
+//         }
+//         else
+//         {
+//             res.status(200).json(obj.data);
+//         }
+//     });
+// }
---
+++ src/routes/requests.js
@@ -0,0 +1,20 @@
+// var express = require('express');
+// var router = express.Router();
+// var auth = require('../controllers/auth');
+// var controller = require('../controllers/requestController');
+
+// router.get("/filter", auth.verifyToken, controller.filter);
+// router.get("/getall", auth.verifyToken, controller.getAll);
+// router.get("/getbyid", auth.verifyToken, controller.getById);
+
+// router.post("/add", auth.verifyToken, controller.add);
+
+// router.delete("/remove", auth.verifyToken, controller.remove);
+
+// router.put("/update", auth.verifyToken, controller.update);
+
+// router.put("/publish", auth.verifyToken, controller.publish);
+// router.put("/unpublish", auth.verifyToken, controller.unPublish);
+// router.put("/archive", auth.verifyToken, controller.archive);
+// router.put("/unarchive", auth.verifyToken, controller.unArchive);
+// module.exports = router;
